18d06c2151d2e39bc3ae3a225baf9efbf9dd412d
AyushR97j <ayushraj803110@gmail.com>
2024-01-20 11:02:55 +0530

    Activity Indicator

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
-import { View, StatusBar } from 'react-native'
+import { useState } from 'react';
+import { View, ActivityIndicator, Button } from 'react-native'
 
 export default function App() {
+  const [ToggleActivityIndicator, setToggleActivityIndicator] = useState(false)
   return (
   <View style={ { flex: 1, backgroundColor: "plum", padding: 60}}>
-    <StatusBar backgroundColor="lightgreen" barStyle="light-content" hidden/>
+    <Button onPress={()=>setToggleActivityIndicator(!ToggleActivityIndicator)} title="Hide/Show Loader"/>
+    <ActivityIndicator animating={ToggleActivityIndicator} />
+    <ActivityIndicator size="large" animating={ToggleActivityIndicator} />
+    <ActivityIndicator size="large" color="midnightblue" animating={ToggleActivityIndicator} />
+    <ActivityIndicator size="extralarge" color="white" animating={ToggleActivityIndicator} />
   </View>
   );
 }
