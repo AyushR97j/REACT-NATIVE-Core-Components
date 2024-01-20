42b45f666c19a5e2c8b9aed4f17b8d2f60cf1e45
AyushR97j <ayushraj803110@gmail.com>
2024-01-20 11:32:45 +0530

    custom component

--- App.js
+++ App.js
@@ -1,28 +1,10 @@
-import { View, Alert, Button } from 'react-native'
+import { View } from 'react-native'
+import Greet from './CoreComponents/components/Greet';
 
 export default function App() {
   return (
   <View style={ { flex: 1, backgroundColor: "plum", padding: 60}}>
-    <Button title="alert 1" onPress={ () => Alert.alert("Alert activated")} />
-    <Button title="alert 2" onPress={ () => Alert.alert("Alert activated 2", "message")} />
-    <Button
-      title="Alert 3"
-      onPress={ () => Alert.alert("Invalid data", "DOB INCORRECT", [
-        {
-          text: "Cancel",
-          onPress : () => console.log("Cancel Pressed"),
-        },
-        {
-          text: "OK",
-          onPress: () => console.log("OK Pressed"),
-        },
-        {
-          text: "Close",
-          onPress: () => console.log("Close Pressed"),
-        },
-      ])
-      }
-    />
+    <Greet name="Ayush"></Greet>
   </View>
   );
 }
